076f90260eb7963ca742b9d9e66a1f9f10f7e9ba
Chitiz Awasthi <chitizawasthi@gmail.com>
2026-09-04 15:24:26 +0530

    update LLM functionality

--- LLM.js
+++ LLM.js
@@ -1,3 +1,33 @@
+// import { GoogleGenAI } from "@google/genai";
+
+// const ai = new GoogleGenAI({
+//   apiKey: process.env.GEMINI_API_KEY
+// });
+
+// async function main() {
+//   const interaction = await ai.interactions.create({
+//     model: "gemini-3.8-flash",
+
+//     input: [
+//       {
+//         type: "user_input",
+//         content: "My name is chitiz."
+//       },
+//       {
+//        type: "model_output",
+//        content: "Hello Chitiz! Nice to meet you. How can I help you today?"
+//       },
+//       {
+//         type: "user_input",
+//         content: "What is my name?"
+//       }
+//     ]
+//   });
+
+//   console.log(interaction.output_text);
+// }
+
+// main();
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({
@@ -7,7 +37,36 @@ const ai = new GoogleGenAI({
 async function main() {
   const interaction = await ai.interactions.create({
     model: "gemini-3.8-flash",
-    input: "what is the capital of France?",
+
+    input: [
+      {
+        type: "user_input",
+        content: [
+          {
+            type: "text",
+            text: "My name is chitiz."
+          }
+        ]
+      },
+      {
+        type: "model_output",
+        content: [
+          {
+            type: "text",
+            text: "Hello Chitiz! Nice to meet you. How can I help you today?"
+          }
+        ]
+      },
+      {
+        type: "user_input",
+        content: [
+          {
+            type: "text",
+            text: "What is my name?"
+          }
+        ]
+      }
+    ]
   });
 
   console.log(interaction.output_text);
